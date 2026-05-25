10576f98d1984c19b03fc3b45f655d78ffb1b5b1
xiongjunyi <3464451913@qq.com>
2026-05-25 11:43:22 +0800

    【Sprint 1】搭建Spring Boot项目框架

---
+++ backend/snake-backend/src/test/java/com/snake/SnakeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
